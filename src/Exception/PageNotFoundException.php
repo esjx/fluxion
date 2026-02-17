70f9bdb19db804db4200070919106f1a0b08f55c
esj <edivan.jr@me.com>
2026-02-17 12:46:31 -0300

    Adjust `PageNotFoundException` to use `LogLevel::WARNING` for improved logging accuracy.

--- src/Exception/PageNotFoundException.php
+++ src/Exception/PageNotFoundException.php
@@ -7,7 +7,7 @@ use Psr\Log\LogLevel;
 class PageNotFoundException extends Exception
 {
 
-    public function __construct(string $page, string $log_level = LogLevel::NOTICE)
+    public function __construct(string $page, string $log_level = LogLevel::WARNING)
     {
         parent::__construct("Página '$page' não encontrada!", [], $log_level);
     }
